3d92de53a53d734c872d5780525bf8df76f90f44
Charles-Martel-739 <hoseina087@gmail.com>
2026-09-08 20:14:55 +0300

    tehtävä 1 on valmis

---
+++ vko2/tehtävä_1/tehtävä1.py
@@ -0,0 +1,24 @@
+summa = 5+5
+print("5+5=",summa)
+
+kertolasku = 20*42
+print("20*42=", kertolasku)
+
+jakolasku = 100/25
+print("100/25=",jakolasku)
+
+vähennyslasku = 756-524
+print("756-524=",vähennyslasku)
+
+potenssi = 4**3
+print("4*3=",potenssi)
+
+jakojäänös = 75%23
+print("75%23=",jakojäänös)
+
+kokonaislukujako = 45//6
+print("45//6=",kokonaislukujako)
+
+jako = 444/11
+print("444/11=",jako)
+
